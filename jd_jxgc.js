51c877726388f5b3c8181ecc6f4a2c6b0a888520
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-05-31 17:51:50 +0800

    Update jd_jxgc.js

--- jd_jxgc.js
+++ jd_jxgc.js
@@ -335,16 +335,19 @@ function rank1(timeout = 0) {
       $.get(url, async (err, resp, data) => {
         try {
           //console.log(url.url)
-          //console.log(data)
+          //console.log(data) startTime":"2021-05-31 00:00:00","
          const result = JSON.parse(data);
          score = data.match(/"score":(.*?),/)[1]
          pm = data.match(/rank":"(.*?)","/)[1]
+         kssj = data.match(/startTime":"(.*?)","/)[1]
+         jssj = data.match(/endTime":"(.*?)","/)[1]
           //$.log(result)
           //$.log(result.score)
           //await notify.sendNotify(`${$.name} - 柠檬jxgc`, `京东账号${$.index} ${$.nickName}`+"电动车制造："+data)
          if (result.errorCode === 0) {
              $.log("\n今日任务已完成\n")
              $.log("\n当前个人积分："+score+"\n当前个人排名："+pm)
+             $.log("\n开始时间："+kssj+"\n结束时间："+jssj)
           //await notify.sendNotify(`${$.name} - 柠檬赚京豆步数换京豆`, `京东账号${$.index} ${$.nickName}`+"\n今日任务已完成\n"+"\n当前个人积分："+data.score+"\n当前个人排名："+data.rank)
             } else {
                //console.log("柠檬赚京豆步数换京豆:2000步"+data.msg)
