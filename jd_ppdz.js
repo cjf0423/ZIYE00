5c5d557dfaa79c00b185c03a58731e846c2b90c3
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-06-01 19:31:53 +0800

    Update jd_ppdz.js

--- jd_ppdz.js
+++ jd_ppdz.js
@@ -368,7 +368,7 @@ function rank(timeout = 0) {
 function showMsg() {
   return new Promise(resolve => {
     message +=  `\n柠檬东东泡泡大战,今日任务已完成\n`+`\n当前个人积分：`+score+`\n当前个人排名：`+pm+`\n开始时间：`+kssj+`\n结束时间：`+jssj
-    if (!jdNotify) {
+    if (!notify.sendNotify) {
       $.msg($.name, '', `${message}`);
     } else {
       $.log(`京东账号${$.index}${$.nickName}\n${message}`);
