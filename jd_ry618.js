7e1bfa7f7377a159707ad4380857d283ec2e61c6
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-06-02 11:44:40 +0800

    Update jd_ry618.js

--- jd_ry618.js
+++ jd_ry618.js
@@ -106,9 +106,10 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 
 
 function dotask(type, id) {
- let body = {"taskType":${type},"taskId":${id}}
+ //let body = {"taskType":${type},"taskId":${id}}
+ let body1 = {"taskType":${type},"taskId":${id}}
     return new Promise(async (resolve) => {
-        const options = taskPostUrl("superbrand_doTask",body)
+        const options = taskPostUrl("superbrand_doTask",body1)
         $.post(options, (err, resp, data) => {
             try {
                 if (err) {
