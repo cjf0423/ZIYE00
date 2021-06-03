a68fac9deb896ff5092a79d10b9c5292978362a5
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-06-03 22:32:37 +0800

    Update jd_xcpp.js

--- jd_xcpp.js
+++ jd_xcpp.js
@@ -1,10 +1,10 @@
 
  [task_local]
- #柠檬东东泡泡大战
- 1 0 * * * https://raw.githubusercontent.com/panghu999/panghu/master/jd_ppdz.js, tag=柠檬东东泡泡大战, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+ #柠檬新潮品牌
+ 1 0 * * * https://raw.githubusercontent.com/panghu999/panghu/master/jd_xcpp.js, tag=柠檬东东泡泡大战, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 */
 
-const $ = new Env('柠檬东东泡泡大战');
+const $ = new Env('柠檬品新潮牌联欢');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 const randomCount = $.isNode() ? 20 : 5;
