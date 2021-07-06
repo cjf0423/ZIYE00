1ea02d90faabf79f03b66a783805f6e3e6f3f84c
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-07-06 19:09:18 +0800

    Update jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -1,3 +1,14 @@
+/*
+入口 京东众筹 许愿池 
+====================
+新手上路勿黑勿喷
+TG通知群 https://t.me/ningmeng999
+微信公众号:柠檬玩机交流
+
+[task_local]
+#柠檬旺财乐园
+0 8,12 * * * 
+*/
 const $ = new Env('柠檬众筹许愿池');
 const notify = $.isNode() ? require("./sendNotify") : "";
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
