d0abfb326f37872e5b96f84389b73080dde3e1aa
panghu999 <51199175+panghu999@users.noreply.github.com>
2021-07-16 01:12:38 +0800

    Update jd_twmsdtt.js

--- jd_twmsdtt.js
+++ jd_twmsdtt.js
@@ -52,11 +52,11 @@ const JD_API_HOST = `https://api.m.jd.com/client.action`;
                 continue
             }
 
-await dotak("29yKWi6sAPG511Jj3TxnjwxibpK7",1000010481)
-await dotak("47JV1WyxCcVbwzXrqWW6BEwuiYhc",1000100813)
-await dotak("352qLFqHdgS6k1K2miiiyUaHnUge",10397406)
-await dotak("3oaCLGRrD1DjNZD9tLkzkequj1zE",1000310642)
-await dotak("29yKWi6sAPG511Jj3TxnjwxibpK7",1000010481)
+await dotask("29yKWi6sAPG511Jj3TxnjwxibpK7",1000010481)
+await dotsk("47JV1WyxCcVbwzXrqWW6BEwuiYhc",1000100813)
+await dotask("352qLFqHdgS6k1K2miiiyUaHnUge",10397406)
+await dotask("3oaCLGRrD1DjNZD9tLkzkequj1zE",1000310642)
+await dotask("29yKWi6sAPG511Jj3TxnjwxibpK7",1000010481)
 for (let i = 0; i < 4; i++) {
 
     await cj()
@@ -76,7 +76,7 @@ if ($.isNode() && allMessage) {
   })
 
 
-async function dotask(encryptAssignmentId,){
+async function dotask(encryptAssignmentId,itemId){
  return new Promise((resolve) => {
      
  let plant6_url = {
